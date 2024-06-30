30943786974b50edcb78fe7d81f0ffdb36866aaf
Archit Jain <jainarchit529@gmail.com>
2024-06-30 19:01:01 +0530

    added

--- src/app.js
+++ src/app.js
@@ -12,13 +12,12 @@ app.use(cors({
 //import routes
 import userRouter from "./routes/user.routes.js"
 
-//routes declaration
-app.use("/api/v1/users", userRouter)
-
-
+// middlewares 
 app.use(express.json({limit: "1024kb"}))
 app.use(express.urlencoded({extended: true, limit: "1024kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes declaration
+app.use("/api/v1/users", userRouter)
 export {app}
